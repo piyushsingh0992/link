cfdcecc4bacc41ca07878100705591d8626bfa89
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-09-19 00:24:56 +0530

    added link for i-phone

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import "./styles.css";
 
 document.getElementById("link").addEventListener("click", () => {
-  if( /Android/i.test(navigator.userAgent ) ) {
+  console.log("z");
+  if (/Android/i.test(navigator.userAgent)) {
+    console.log("a");
     // If the user is using an Android device.
-    setTimeout(function () { window.location = "market://details?id=com.google.android.youtube"; }, 25);
+    setTimeout(function () {
+      window.location = "https://www.youtube.com/watch?v=YGn1UlQ3osE";
+    }, 25);
     window.location = "vnd.youtube://www.youtube.com/watch?v=YGn1UlQ3osE";
+  } 
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    
 }
+else {
+    setTimeout(function () {
+        window.location = "https://www.youtube.com/watch?v=YGn1UlQ3osE";
+      }, 25);
+      window.location = "youtube://www.youtube.com/watch?v=YGn1UlQ3osE";
+  }
+
+  console.log("c");
 });
