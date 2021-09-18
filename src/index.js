3a5fa7bd10733543b37e466b1912343c845069a9
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-09-18 22:23:02 +0530

    added android link

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
 document.getElementById("link").addEventListener("click", () => {
-  window.open("https://www.w3schools.com");
+  if( /Android/i.test(navigator.userAgent ) ) {
+    // If the user is using an Android device.
+    setTimeout(function () { window.location = "market://details?id=com.google.android.youtube"; }, 25);
+    window.location = "vnd.youtube://www.youtube.com/watch?v=YGn1UlQ3osE";
+}
 });
